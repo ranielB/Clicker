101f7ac75755a029b0778ee484b115fd1f040701
ranielB <70814418+ranielB@users.noreply.github.com>
2023-06-18 23:34:51 +0200

    update1
    update

--- public/assets/cuchilla.js
+++ public/assets/cuchilla.js
@@ -6,6 +6,8 @@ const div_dps = document.getElementById("dps");
 const div_golpes = document.getElementById("golpes"); 
 const cuchillo = document.getElementById("cuchillo"); 
 const furry = document.getElementById("furry"); 
+const descarga = document.getElementById("descarga"); 
+const pene = document.getElementById("pene"); 
 const ajustes = document.getElementById("ajustes"); 
 const pantalla_ajustes = document.getElementById("pantalla_ajustes"); 
 const add_cuchilla = document.getElementById("add_cuchilla");
@@ -19,6 +21,8 @@ const opciones = document.querySelectorAll('.opcion');
 
 let cuchillas = 0;
 let lvl_cuchillo = 1;
+let lvl_electrocutar = 1;
+let lvl_cortar = 1;
 let dinero = 0;
 let dps = 0;
 let golpes = 0;
@@ -34,12 +38,20 @@ function actualizar() {
     mostrar_estadisticas();
 
     //cambiar furrysprite
-    if(golpes > 50){
-        furry.classList = "dos";
+    if(lvl_cortar > 1){
+        furry.classList = "cinco";
     }
-    if(golpes > 100){
+    else if(golpes > 10000){
+        furry.classList = "cuatro";
+    }
+    else if(golpes > 1000){
         furry.classList = "tres";
     }
+    else if(golpes > 100){
+        furry.classList = "dos";
+    }
+    
+    
 }
   
   // Configurar la ejecución de la función 60 veces por segundo
@@ -65,13 +77,14 @@ torture_room.onmouseup= function(){
 }
 btns[0].addEventListener("click", function(){
     if(dinero >= 10*(cuchillas+1)){
-        circulo_cuchilla.innerHTML = "";
         cuchillas++;
         dps += 0.5;
         dinero = dinero -10*(cuchillas);
         btns[0].innerHTML = "Añadir cuchilla("+(10*(cuchillas+1))+")";
 
-        for(let i=0; i<cuchillas;i++){
+        //VISUAL
+        circulo_cuchilla.innerHTML = "";    //reset necesario
+        for(let i=0; i<cuchillas;i++){      //imprime las cuchillas de nuevo
             const cuchilla = new Cuchilla(360*i/cuchillas);
         }
         mostrar_estadisticas();
@@ -86,6 +99,29 @@ btns[1].addEventListener("click", function(){
         mostrar_estadisticas();
     }
     
+});
+btns[2].addEventListener("click", function(){
+    if(dinero >= (1000 * Math.pow(2, (lvl_electrocutar-1)))){
+        lvl_electrocutar++;
+        descarga.style.display = "block";
+        dps+=50;
+        dinero = dinero-(1000 * Math.pow(2, (lvl_electrocutar-2)));
+        btns[2].innerHTML = "Mejorar potencia(" + (1000 * Math.pow(2, (lvl_electrocutar-1)))+")";
+        mostrar_estadisticas();
+    }
+});
+btns[3].addEventListener("click", function(){
+    if(dinero >= (100000 * Math.pow(2, (lvl_cortar-1)))){
+        lvl_cortar++;
+        dps+=1000;
+        pene.classList.add("cortado");
+        setTimeout(function(){
+            pene.classList.remove("cortado");
+        }, 2000);
+        dinero = dinero-(100000 * Math.pow(2, (lvl_cortar-2)));
+        btns[2].innerHTML = "Mejorar potencia(" + (100000 * Math.pow(2, (lvl_cortar-1)))+")";
+        mostrar_estadisticas();
+    }
 });
 
 
